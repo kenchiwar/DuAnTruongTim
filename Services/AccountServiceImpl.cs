cfc23e13f9678d77789cf50ab422961ebcc1efd6
LeToaan <thaitoan553@gmail.com>
2023-07-10 19:11:03 +0700

    sua service

--- Services/AccountServiceImpl.cs
+++ Services/AccountServiceImpl.cs
@@ -32,26 +32,62 @@ public class AccountServiceImpl : AccountService
             schoolyear = account.Schoolyear,
             degree = account.Degree,
             academicrank = account.Academicrank,
-            role = account.IdRoleNavigation,
-            department = account.IdDepartmentNavigation,
-            requetIdComplainNavigations = account.RequetIdComplainNavigations.Select(request => new
+            role = account.Role,
+            idDepartmentNavigation = new
+            {
+                account.IdDepartmentNavigation.Id,
+                account.IdDepartmentNavigation.TenDepartment,
+                account.IdDepartmentNavigation.Describe,
+                account.IdDepartmentNavigation.Address,
+                account.IdDepartmentNavigation.Status,
+            },
+            idRoleNavigation = new {
+                account.IdRoleNavigation.Id,
+                account.IdRoleNavigation.Name,
+                account.IdRoleNavigation.Describe,
+            },
+            requetIdComplainNavigations = account.RequetIdComplainNavigations.Select(requestComplainNavigation => new
             {
         
-                id = request.Id,
-                idComplain = request.IdComplain,
-                idDepartment = request.IdDepartment,
-                idHandle = request.IdHandle,
-                title = request.Title,
-                status = request.Status,
-                level = request.Level,
-                sentDate = request.Sentdate,
-                endDate = request.Enddate,
-                priority = request.Priority,
-                complainUsername = request.IdComplainNavigation,
-                departmentName = request.IdDepartmentNavigation,
-                handleUsername = request.IdHandleNavigation,
+                id = requestComplainNavigation.Id,
+                idComplain = requestComplainNavigation.IdComplain,
+                idDepartment = requestComplainNavigation.IdDepartment,
+                idHandle = requestComplainNavigation.IdHandle,
+                title = requestComplainNavigation.Title,
+                status = requestComplainNavigation.Status,
+                level = requestComplainNavigation.Level,
+                sentDate = requestComplainNavigation.Sentdate,
+                endDate = requestComplainNavigation.Enddate,
+                priority = requestComplainNavigation.Priority,
+                complainUsername = requestComplainNavigation.IdComplainNavigation,
+                departmentName = requestComplainNavigation.IdDepartmentNavigation,
+                handleUsername = requestComplainNavigation.IdHandleNavigation,
 
-            })
+            }),
+            requetIdHandleNavigations = account.RequetIdHandleNavigations.Select(requestHandleNavigation => new
+            {
+                id = requestHandleNavigation.Id,
+                idComplain = requestHandleNavigation.IdComplain,
+                idDepartment = requestHandleNavigation.IdDepartment,
+                idHandle = requestHandleNavigation.IdHandle,
+                title = requestHandleNavigation.Title,
+                status = requestHandleNavigation.Status,
+                level = requestHandleNavigation.Level,
+                sentDate = requestHandleNavigation.Sentdate,
+                endDate = requestHandleNavigation.Enddate,
+                priority = requestHandleNavigation.Priority,
+                complainUsername = requestHandleNavigation.IdComplainNavigation,
+                departmentName = requestHandleNavigation.IdDepartmentNavigation,
+                handleUsername = requestHandleNavigation.IdHandleNavigation,
+            }),
+            idRoleClaim = account.IdRoleClaims.Select(idRoleClaim => new
+            {
+                id = idRoleClaim.Id,
+                name = idRoleClaim.Name,
+                describe = idRoleClaim.Describe,
+                claim = idRoleClaim.Claim,
+            }),
+
         }).ToListAsync();
     }
 }
