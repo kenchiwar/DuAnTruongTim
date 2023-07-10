e5113e8f995e622184f12c0262a027d97f620b7a
LeToaan <thaitoan553@gmail.com>
2023-07-10 15:08:29 +0700

    them vao appjson.development

--- Program.cs
+++ Program.cs
@@ -9,7 +9,11 @@ builder.Services.AddCors();
 var connectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
 builder.Services.AddDbContext<CheckQlgiaoVuContext>(option => option.UseLazyLoadingProxies().UseSqlServer(connectionString));
 builder.Services.AddScoped<AccountService, AccountServiceImpl>();
-builder.Services.AddScoped<DepartmentService, AccountServiceImpl>();
+builder.Services.AddScoped<DepartmentService, DepartmentServiceImpl>();
+builder.Services.AddScoped<RequestService, RequestServiceImpl>();
+builder.Services.AddScoped<RoleClaimService, RoleClaimServiceImpl>();
+builder.Services.AddScoped<RoleService, RoleServiceImpl>();
+
 var app = builder.Build();
 app.MapControllers();
 app.UseCors(builder => builder .AllowAnyHeader() .AllowAnyMethod() .SetIsOriginAllowed((host) => true) .AllowCredentials() );
